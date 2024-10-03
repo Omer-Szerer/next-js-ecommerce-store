da1673a24e250355220d18a3d2b8ce38cafff4ab
Omer Szerer <omerszerer@gmail.com>
2024-10-03 13:53:17 +0200

    Add products page and a metadata template

---
+++ app/products/page.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Products',
+  description: 'Super quality Austrian chocolate',
+};
+
+export default function ProductsPage() {
+  return <h1>Products Page</h1>;
+}
