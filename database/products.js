83a2d6a5d80eff51883cb0870bd4b572fcca5ece
Omer Szerer <omerszerer@gmail.com>
2024-10-03 14:14:39 +0200

    Add products database

---
+++ database/products.js
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Spooky Choco',
+    price: 5,
+    vegan: false,
+    alcoholFree: true,
+  },
+  {
+    id: 2,
+    name: 'Strawberry Monster',
+    price: 5,
+    vegan: false,
+    alcoholFree: true,
+  },
+  {
+    id: 3,
+    name: 'Pumpkin Seeds with Marzipan',
+    price: 5,
+    vegan: false,
+    alcoholFree: false,
+  },
+  {
+    id: 4,
+    name: 'Pumpkin Caramel and Strawberries',
+    price: 5,
+    vegan: false,
+    alcoholFree: false,
+  },
+];
