c342360476cf794d99823e50c9e0cb8be8492b0d
Omer Szerer <omerszerer@gmail.com>
2024-10-10 17:14:32 +0200

    Add connect.ts file

--- database/productsDatabase.js
+++ database/productsDatabase.js
@@ -5,10 +5,6 @@ config();
 
 const sql = postgres();
 
-// const sql = postgres(
-//   'postgres://next_js_ecommerce_store:next_js_ecommerce_store@localhost/next_js_ecommerce_store',
-// );
-
 console.log(
   await sql`
   SELECT
