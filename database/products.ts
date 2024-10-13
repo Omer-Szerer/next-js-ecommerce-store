aef956db22eefdb47bac2b8b8c1ef5ef81606c01
Omer Szerer <omerszerer@gmail.com>
2024-10-13 09:35:00 +0200

    Delete fake dtabase file

--- database/products.ts
+++ database/products.ts
@@ -44,3 +44,47 @@ export const getProductInsecure = cache(async (id: number) => {
 `;
   return product;
 });
+
+// Fake Database
+// export const products = [
+//   {
+//     id: 1,
+//     name: 'Spooky Choco',
+//     price: 4.2,
+//     description: 'dummy text',
+//     vegan: false,
+//     alcoholFree: true,
+//   },
+//   {
+//     id: 2,
+//     name: 'Strawberry Monster',
+//     price: 4.2,
+//     description: 'dummy text',
+//     vegan: false,
+//     alcoholFree: true,
+//   },
+//   {
+//     id: 3,
+//     name: 'Pumpkin Seeds with Marzipan',
+//     price: 5.3,
+//     description: 'dummy text',
+//     vegan: false,
+//     alcoholFree: false,
+//   },
+//   {
+//     id: 4,
+//     name: 'Pumpkin Caramel and Strawberries',
+//     price: 4.7,
+//     description: 'dummy text',
+//     vegan: false,
+//     alcoholFree: false,
+//   },
+// ];
+
+// export function getProducts() {
+//   return products;
+// }
+
+// export function getProduct(id) {
+//   return products.find((product) => product.id === id);
+// }
