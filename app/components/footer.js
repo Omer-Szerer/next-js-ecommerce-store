84aaa4c5cd0a646524fd1f0fc8c0a018088a4fd2
Omer Szerer <omerszerer@gmail.com>
2024-10-18 08:23:30 +0200

    Update footer

--- app/components/footer.js
+++ app/components/footer.js
@@ -4,7 +4,12 @@ export default function Footer() {
   return (
     <footer className="fixed_footer">
       <div className="content">
-        <p>All images are credited to Zotter Schokolade.</p>
+        <p>
+          This website is a full-stack example project of an e-commerce
+          platform.
+          <br />
+          <br /> All images are credited to Zotter Schokolade.
+        </p>
       </div>
     </footer>
   );
